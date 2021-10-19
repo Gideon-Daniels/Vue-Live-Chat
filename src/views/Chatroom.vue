00d37f3e6398ffe60861674fbf72349b16bfadcc
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-19 15:56:21 +0200

    update

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,10 +1,14 @@
 <template>
-  <p>Chat Room</p>
+  <div class="container">
+      <Navbar/>
+  </div>
 </template>
 
 <script>
-export default {
+import Navbar from '../components/Navbar.vue'
 
+export default {
+    components: { Navbar }
 }
 </script>
 
